b803b40afa574064f2793b8f3cb6be80f3a3623a
LinhPT <linhpt.uet@gmail.com>
2020-09-15 22:07:22 +0700

    create data jpa models

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,20 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+}
